09cc7959445d48c9dc1b968ada2d071bf59d45d2
Carlos Mijail Mamani Anccasi <89269010+CharlsMikhail@users.noreply.github.com>
2024-05-21 10:58:36 -0500

    fragment_pay.xml, se avanzo con aprox la mitad, por ahora todo bien!

---
+++ TPAGO_APP/app/src/main/java/com/example/tpago/PayFragment.kt
@@ -0,0 +1,13 @@
+package com.example.tpago
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class PayFragment : Fragment(R.layout.fragment_pay) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
