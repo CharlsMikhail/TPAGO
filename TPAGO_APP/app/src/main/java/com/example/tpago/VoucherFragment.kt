7a3db9d9f142f1e62375af6bdd92f74c8ad62fb8
Carlos Mijail Mamani Anccasi <89269010+CharlsMikhail@users.noreply.github.com>
2024-05-21 19:14:32 -0500

    Se avanzo con un 50% del fragment_voucher.xml, falta todo el detalle, me parece que tiene que resumirse

---
+++ TPAGO_APP/app/src/main/java/com/example/tpago/VoucherFragment.kt
@@ -0,0 +1,12 @@
+package com.example.tpago
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+
+class VoucherFragment : Fragment(R.layout.fragment_voucher) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
