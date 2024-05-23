5b76b96a1d4eb5bbdba38fa117074d6f75f088a1
Carlos Mijail Mamani Anccasi <89269010+CharlsMikhail@users.noreply.github.com>
2024-05-22 22:31:08 -0500

    initial commit

---
+++ TPAGO_APP2/app/src/main/java/com/example/tpago2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tpago2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
